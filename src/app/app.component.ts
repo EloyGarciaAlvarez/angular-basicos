284bafa8a015c7cda4286cbd695769488b80f92b
EloyGarciaAlvarez <egaral81@gmail.com>
2022-05-22 20:50:34 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+
+// Importación del decorador Component de @angular/core
 import { Component } from '@angular/core';
 
+/*El argumento del decorador Component es un objeto con varios campos.
+selector: es la equita html que representa al componente. En este caso app-root. Podemos ver que se usa en el fichero index.html.
+template: La plantilla html del componente.
+templateURL: La referencia al archivo html del componente.
+styles: Estilos para el componente.
+stylesUrls: Array con Referencias ficheros CSS de estilos para el componente.
+*/
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+  
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,45 @@
+
+/* Creamos un nuevo Componente , en este caso un componente para el Contador .
+Para usarlo:
+1. Añadimos la etiqueta <app-contador></app-contador> al app.component.html.
+2. Añadirmos el nuevo componente al módulo. en app.module.ts, en declarations, añadimos en ContadorComponent.  */
+
+
+import { Component, ComponentRef } from "@angular/core";
+
+@Component({
+    selector: 'app-contador',
+    template: `
+        <h2>Componente contador</h2>
+        <h3>La base es: {{base}}</h3>
+        <!-- Desde el template tenemos acceso a la clase del componente -->
+        <button (click)="variarContador(true);"> + {{base}} </button>
+        <button (click)="modificarContador('inc');"> + {{base}} </button>
+        <button (click)="incrementarContador();"> + {{base}} </button>
+        <span>{{contador}}</span>
+        <button (click)="contador = contador -base;"> - {{base}} </button>
+        <button (click)="modificarContador('dec');"> - {{base}} </button>
+        <button (click)="incDecContador(-base);"> - {{base}} </button>
+    `,
+    styles: ['color:blue']
+})
+
+export class ContadorComponent{
+  public titulo: string = 'Contador App';
+  public contador: number = 0;
+  public base: number = 3;
+
+  public incrementarContador=():void=>{
+      this.contador+=this.base;
+  }
+
+  public incDecContador=(cantidad:number):void=>{
+      this.contador+=cantidad;
+  }
+  public variarContador=(operacion:boolean):void=>{
+    operacion?this.contador+=this.base:this.contador-=this.base;
+  }
+  public modificarContador=(operacion:string)=>{
+    operacion=="inc"?this.contador+=this.base:this.contador-=this.base;
+  }
+}
